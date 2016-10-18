ba3608a134f733bf6dc3f6daf222924d684c264b
Etheya <brian@etheya-digitalcreativity.com>
2016-10-18 09:33:36 +0100

    linkup of pre application section

--- app/routes.js
+++ app/routes.js
@@ -20,3 +20,32 @@ router.get('/', function (req, res) {
 // add your routes here
 
 module.exports = router
+
+
+// Business matching branching
+
+router.get('/sprint27/business-structure', function (req, res) {
+    var structure = req.query.structure;
+    if (structure === 'bd') {
+        res.redirect('/sprint27/business-details');
+    } else if (structure === 'bst') {
+        res.redirect('/sprint27/business-details-st');
+    } else if (structure === 'bdp'){
+        res.redirect('/sprint27/business-details-partner');
+    } else if (structure === 'bdlp'){
+        res.redirect('/sprint27/business-details-limited-partner');
+    } else {
+        res.render('sprint27/business-structure')
+    }
+
+})
+
+
+
+// router.get('*', function (req, res, next) {
+//       if (req.query['next-page']) {
+//         res.redirect(name + '/' + req.query['next-page']);
+//       } else {
+//         next();
+//       }
+//     });
